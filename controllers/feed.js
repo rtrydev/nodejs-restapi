3605875e1e94b4286e0e6d13875c1915d1967384
rtrydev <r.kulka44@gmail.com>
2022-06-05 18:43:00 +0200

    clear relation on item delete

--- controllers/feed.js
+++ controllers/feed.js
@@ -172,7 +172,13 @@ exports.deletePost = (req, res, next) => {
       return Post.findByIdAndRemove(postId);
     })
     .then((result) => {
-      console.log(result);
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      user.posts.pull(postId);
+      return user.save();
+    })
+    .then((result) => {
       res.status(200).json({ message: "Item deleted" });
     })
     .catch((err) => {
