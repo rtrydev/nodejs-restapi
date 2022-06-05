9444177fabf030baecf596f63ce857f580c417b0
rtrydev <r.kulka44@gmail.com>
2022-06-05 16:54:29 +0200

    add user model, empty controller

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const feedRoutes = require("./routes/feed");
+const authRoutes = require("./routes/auth");
 const multer = require("multer");
 
 const app = express();
@@ -48,6 +49,7 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRoutes);
+app.use("/auth", authRoutes);
 
 app.use((error, req, res, next) => {
   console.log(error);
